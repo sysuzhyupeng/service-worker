15c0434d4c37811af68a1d6264db516fcb2ee37e
sysuzhyupeng <673341297@qq.com>
2017-10-08 19:19:36 +0800

    add cache

--- app/index.js
+++ app/index.js
@@ -1,3 +1,14 @@
+/*
+    虽然又调了一次注册，但并不会重新注册，它发现"sw.js"这个已经注册了，
+    就不会再注册了，进而不会触发install和active事件，因为当前Service Worker已经是active状态了。
+    当需要更新Service Worker时，如变成"sw-1.js"，或者改变sw.js的文本内容，就会重新注册，
+    新的Service Worker会先install然后进入waiting状态，等到重启浏览器时，老的Service Worker就会被替换掉，
+    新的Service Worker进入active状态
+*/
+/*
+    其中Cache Storage是指Service Worker和Manifest占用的空间大小和，
+    总的空间大小是20GB，几乎是unlimited，所以基本上不用担心缓存会不够用。
+*/
 window.addEventListener("load", function() {
     console.log("Will the service worker register?");
     navigator.serviceWorker.register('/sw.js')
--- sw.js
+++ sw.js
@@ -1,15 +1,57 @@
-const CACHE_NAME = "fed-cache";
+const CACHE_NAME = "sw-cache";
 this.addEventListener("install", function(event) {
     this.skipWaiting();
     console.log("install service worker");
     // 创建和打开一个缓存库
     caches.open(CACHE_NAME);
     // 首页
-    let cacheResources = ["https://www.baidu.com"];
     event.waitUntil(
         // 请求资源并添加到缓存里面去
         caches.open(CACHE_NAME).then(cache => {
-            cache.addAll(cacheResources);
+            return cache.addAll([
+                'resources/img/1.jpg',
+                'resources/img/2.jpg',
+                'resources/img/3.jpg',
+                'resources/less/index.css'
+              ]);
         })
     );
 });
+
+
+let util = {
+    fetchPut: function (request, callback) {
+        return fetch(request).then(response => {
+            // 跨域的资源直接return
+            if (!response || response.status !== 200 || response.type !== "basic") {
+                return response;
+            }
+            this.putCache(request, response.clone());
+            typeof callback === "function" && callback();
+            return response;
+        });
+    },
+    putCache: function (request, resource) {
+        // 后台不要缓存，preview链接也不要缓存
+        if (request.method === "GET" && request.url.indexOf("wp-admin") < 0 
+              && request.url.indexOf("preview_id") < 0) {
+            caches.open(CACHE_NAME).then(cache => {
+                cache.put(request, resource);
+            });
+        }
+    }
+};
+
+this.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            //缓存命中
+            // cache hit
+            if (response) {
+                return response;
+            }
+            return util.fetchPut(event.request.clone());
+        })
+    );
+});
+
